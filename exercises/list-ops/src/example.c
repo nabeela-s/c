663d1715a7dc8683fab4b2b1cb96cedca30d63d3
nabeela-s <nabeela_s@bivlab.com>
2021-01-01 12:55:21 +0000

    ...

--- exercises/list-ops/src/example.c
+++ exercises/list-ops/src/example.c
@@ -35,9 +35,9 @@ list_t *append_list(list_t * list1, list_t * list2)
    return list;
 }
 
-list_t *filter_list(list_t * list, bool(*filter) (list_value_t value))
+list_t *filter_list(list_t * list, bool (*filter)(list_value_t value))
 {
-   if (!list || !filter)
+   if(!list || !filter)
       return NULL;
 
    list_t *filtered =
